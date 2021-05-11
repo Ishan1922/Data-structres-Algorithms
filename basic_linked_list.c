f4899b5137e8a2e732011b64f37680ef4abfc541
Ishan Vadalia <ishanvadalia1607@gmail.com>
2021-05-12 00:13:58 +0530

    basic linked list ceated

---
+++ basic_linked_list.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void insertatend();
+void deleteaparticular();
+void display();
+
+struct node *head = NULL;
+
+int main()
+{
+    insertatend();
+    insertatend();
+    deleteaparticular(44);
+    insertatend();
+    display();
+    return 0;
+}
+
+void insertatend()
+{
+    struct node *temp, *ptr;
+    temp = (struct node *)malloc(sizeof(struct node *));
+    printf("enter the element : ");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+    if (head == NULL)
+    {
+        head = temp;
+    }
+    else
+    {
+        ptr = head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = temp;
+    }
+}
+
+void display()
+{
+    struct node *ptr;
+    ptr = head;
+    if (ptr == NULL)
+        printf("list is empty");
+    printf("the list is as follows...\n");
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+void deleteaparticular(int val)
+{
+    struct node *ptr, *prevptr;
+    ptr = head;
+    while (ptr->data != val && ptr->next != NULL)
+    {
+        prevptr = ptr;
+        ptr = ptr->next;
+    }
+    if (ptr->data != val)
+    {
+        printf("\nvalue doesn't exist in the list\n");
+    }
+    else
+    {
+        prevptr->next = ptr->next;
+        free(ptr);
+    }
+}
